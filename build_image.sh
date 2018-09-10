8f44036cf6e5297b0207e9718006427999ed9640
Naresh Khalasi <naresh.khalasi@gmail.com>
2018-09-10 23:51:29 +0530

    Minor fixes to helper scripts after pulling changes from upstream

--- build_image.sh
+++ build_image.sh
@@ -8,7 +8,7 @@ docker build --rm --compress --no-cache --tag $IMAGE_NAME $PWD
 
 docker run -t --name $IMAGE_NAME-instance $IMAGE_NAME:latest /bin/true
 
-docker export `docker ps -q -n=1` | docker import --change 'ENV ROOT_CN="Root"' --change 'ENV ROOT_NAME="root"' --change 'ENV CERT_DIR="/etc/ssl/certs"' --change 'VOLUME ["/etc/ssl/certs"]' --change 'ENTRYPOINT ["/entrypoint.sh"]' - $IMAGE_NAME:stripped
+docker export `docker ps -q -n=1` | docker import --change 'ENV ROOT_CN="Root"' --change 'ENV ROOT_NAME="root"' --change 'ENV CERT_DIR="/etc/ssl/certs"' --change 'VOLUME ["/etc/ssl/certs"]' --change 'ENTRYPOINT ["/docker-entrypoint.sh"]' - $IMAGE_NAME:stripped
 
 docker rm $IMAGE_NAME-instance
 docker rmi $IMAGE_NAME:latest
--- issue_certificate.sh
+++ issue_certificate.sh
@@ -9,4 +9,4 @@ then
     exit
 fi
 
-docker run --name openssl-alpine --rm -e COUNTY="IN" -e STATE="MAHARASHTRA" -e LOCATION="PUNE" -e ORGANISATION="Vayana Network" -e ISSUER_CN="VNSPL-CA" -e ISSUER_NAME="VNSPL-CA" -e PUBLIC_CN="$1" -e PUBLIC_NAME="$1" -e RSA_KEY_NUMBITS="2048" -e DAYS="365" -v $PWD/internal-ca:/etc/ssl/certs:rw openssl-alpine:stripped
+docker run --name openssl-alpine --rm -e COUNTY="IN" -e STATE="MAHARASHTRA" -e LOCATION="PUNE" -e ORGANISATION="Vayana Network" -e ISSUER_CN="VNSPL-CA" -e ISSUER_NAME="VNSPL-CA" -e PUBLIC_CN="$1" -e PUBLIC_NAME="$1" -e KEYSTORE_NAME="$1" -e RSA_KEY_NUMBITS="2048" -e DAYS="365" -v $PWD/internal-ca:/etc/ssl/certs:rw openssl-alpine:stripped
