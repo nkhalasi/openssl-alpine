a54efef463eb7ec64894868890b8f08661f3bdc5
Naresh Khalasi <naresh.khalasi@gmail.com>
2018-09-10 23:31:55 +0530

    Added cmd line params check in the issue scripts

--- issue_certificate.sh
+++ issue_certificate.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
+usage()
+{
+    echo "$0 <serverdomain>"
+}
+if [ $# != 1 ]
+then
+    usage
+    exit
+fi
 
 docker run --name openssl-alpine --rm -e COUNTY="IN" -e STATE="MAHARASHTRA" -e LOCATION="PUNE" -e ORGANISATION="Vayana Network" -e ISSUER_CN="VNSPL-CA" -e ISSUER_NAME="VNSPL-CA" -e PUBLIC_CN="$1" -e PUBLIC_NAME="$1" -e RSA_KEY_NUMBITS="2048" -e DAYS="365" -v $PWD/internal-ca:/etc/ssl/certs:rw openssl-alpine:stripped
--- issue_user_certificate.sh
+++ issue_user_certificate.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
+usage()
+{
+    echo "$0 <username> <firstname-lastname> <email>"
+}
+if [ $# != 3 ]
+then
+    usage
+    exit
+fi
+
 
 docker run --name openssl-alpine --rm -e COUNTY="IN" -e STATE="MAHARASHTRA" -e LOCATION="PUNE" -e ORGANISATION="Vayana Network" -e ISSUER_CN="VNSPL-CA" -e ISSUER_NAME="VNSPL-CA" -e USERCERTFILE="$1" -e USERNAME="$2" -e USEREMAIL="$3" -e RSA_KEY_NUMBITS="2048" -e DAYS="3650" -v $PWD/internal-ca:/etc/ssl/certs:rw openssl-alpine:stripped
